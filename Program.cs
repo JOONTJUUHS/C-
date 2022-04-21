dd07868db1ee2a51989cb2dae9ed9f0cdf99bc8b
JOONTJUUHS <71132375+JOONTJUUHS@users.noreply.github.com>
2022-04-21 11:28:58 +0200

    functies

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Functies.py
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            def add(getal1, getal2):
+             print(getal1+getal2)
+
+                add(4, 8)
+
+
+
+
+        }
+    }
+}
